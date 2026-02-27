63f88640baee593d416fde9b54045cf20232581f
Prachiwasu27 <wasuprachi@gmail.com>
2026-02-27 22:47:28 +0530

    Fix import path for aiController in aiRoutes.js

--- aiRoutes.js
+++ aiRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { generateResponse } = require("../controllers/aiController");
+const { generateResponse } = require('./aiController');
 
 router.post("/generate", generateResponse);
 
 module.exports = router;
+
